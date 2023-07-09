b907bfb7e416046d0df2f9017488b5fc848f2b02
PranshuPandey90 <138874525+PranshuPandey90@users.noreply.github.com>
2023-07-09 14:22:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Webapp from './components/Webapp';
+
+function App() {
+  return (
+    <div className="App">
+      <Webapp/>
+    </div>
+  );
+}
+
+export default App;
